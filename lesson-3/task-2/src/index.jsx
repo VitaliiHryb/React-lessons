fd629ed68c62866d91fef745993ac28688ab9767
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-12 20:39:11 +0300

    Классовые компоненты

--- lesson-3/task-2/src/index.jsx
+++ lesson-3/task-2/src/index.jsx
@@ -1,24 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Search from './Search.jsx';
 import './index.scss';
-import './search.scss';
+// import './search.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class SearchField extends Component {
-  render() {
-    return (
-      <div className="search">
-        <h1 className="search__title">
-          {`Hello, ${this.props.name}. What to search for you?`}
-        </h1>
-        <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-root.render(<SearchField name="Bob" />);
+root.render(<Search name="Tom" />);
