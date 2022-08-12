c045df145b0c1800b6fcb791ff227afcccbfca2e
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-12 20:06:44 +0300

    Функциональные компоненты

--- lesson-3/task-1/src/index.jsx
+++ lesson-3/task-1/src/index.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Search from './Search.jsx';
 import './index.scss';
-import './search.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// const style = { color: 'red', fontWeight: '700' };
-
-export default Search = props => {
-  // console.log(props);
-  return (
-    <div className="search">
-      <h1 className="search__title">
-        {`Hello, ${props.name}. What to search for you?`}
-      </h1>
-      <div className="search__field">
-        <input type="text" className="search__input" />
-        <button className="search__button">Search</button>
-      </div>
-    </div>
-  );
-};
 
 root.render(<Search name="Bob" />);
