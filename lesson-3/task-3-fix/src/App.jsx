27c78a07e54c8271a20025e7d1a0f300f4039b5f
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-15 20:13:47 +0300

    without autor

---
+++ lesson-3/task-3-fix/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Comment from './Comment';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://miro.medium.com/max/555/1*Om2kCjnIJM4QT0wPqgdp2g.png',
+};
+
+const App = () => {
+  return (
+    <Comment
+      user={userInfo}
+      text="Good job"
+      date={new Date('2019-01-01T11:32:19.566Z')}
+    />
+  );
+};
+
+export default App;
