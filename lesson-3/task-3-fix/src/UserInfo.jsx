2334a749ca2fe8c1046874940ce11575eef6b5ff
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-15 20:59:50 +0300

    Композиция компонентов task-3

--- lesson-3/task-3-fix/src/UserInfo.jsx
+++ lesson-3/task-3-fix/src/UserInfo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import UserAvatar from './Avatar';
+import Avatar from './Avatar';
 
 const UserInfo = props => {
   return (
     <div className="user-info">
-      <UserAvatar avatarUrl={props.user.avatarUrl} author={props.user.name} />
+      <Avatar avatarUrl={props.user.avatarUrl} author={props.user.name} />
 
       <div className="user-info__name">{props.user.name}</div>
     </div>
