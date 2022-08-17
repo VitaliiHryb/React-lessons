95deead06c26ee1ed6ab7488e8da21408b8d152a
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-17 17:44:11 +0300

    Счетчик

--- lesson-5/task-3/src/Counter.jsx
+++ lesson-5/task-3/src/Counter.jsx
@@ -2,12 +2,44 @@ import React, { Component } from 'react';
 
 // class GoodButton extends React.Component
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+
+    this.decrement = this.decrement.bind(this);
+    // this.increment = this.increment.bind(this);
+  }
+
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+
+  // increment() {
+  //   this.setState({
+  //     counter: this.state.counter + 1,
+  //   });
+  // }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
   render() {
     return (
       <div className="counter">
-        <button className="counter__button">-</button>
-        <span className="counter__value">0</span>
-        <button className="counter__button">+</button>
+        <button className="counter__button" onClick={this.decrement}>
+          -
+        </button>
+        <span className="counter__value">{this.state.counter}</span>
+        <button className="counter__button" onClick={this.increment}>
+          +
+        </button>
       </div>
     );
   }
