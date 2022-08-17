9181a96173d835af3e5ca106ca779016c5ab5e1f
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-17 21:24:48 +0300

    Arguments to the handler

---
+++ lesson-5/task-4/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import Colors from './Colors';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Colors />);
