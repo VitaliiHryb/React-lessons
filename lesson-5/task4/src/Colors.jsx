d6341488f3b8fdc9583e3aa93f046283326f4d0d
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-17 23:11:30 +0300

    try to fix Arguments to the handler

---
+++ lesson-5/task4/src/Colors.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './index.css';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+// class GoodButton extends React.Component
+class Colors extends Component {
+  setBodyColor = color => {
+    // console.log(e);
+    document.body.style.backgroundColor = color;
+  };
+
+  render() {
+    return (
+      <div className="colors">
+        <button
+          style={{ backgroundColor: RED }}
+          onClick={() => this.setBodyColor(RED)}
+          className="colors-button"
+        />
+        <button
+          style={{ backgroundColor: GREEN }}
+          onClick={() => this.setBodyColor(GREEN)}
+          className="colors-button"
+        />
+        <button
+          style={{ backgroundColor: BLUE }}
+          onClick={() => this.setBodyColor(BLUE)}
+          className="colors-button"
+        />
+      </div>
+    );
+  }
+}
+
+export default Colors;
---
+++ lesson-5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.scss';
+import './index.css';
+import Colors from './Colors';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Colors />);
