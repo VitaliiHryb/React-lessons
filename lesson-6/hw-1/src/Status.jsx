05071730238aa037f23ca8b5738de20e1fe27378
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-22 20:42:52 +0300

    Offline / Online status

---
+++ lesson-6/hw-1/src/Status.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+// const text1 = 'Hello, world!';
+// const text2 = 'Another exciting text.';
+
+class Status extends Component {
+  state = {
+    isOnline: false,
+  };
+
+  setText = isOnline => {
+    this.setState({
+      isOnline,
+    });
+  };
+
+  render() {
+    return (
+      <div className="status">
+        {this.state.isOnline ? <Online /> : <Offline />}
+      </div>
+    );
+  }
+}
+
+export default Status;
