0f3eb4d4cb96b7c264b4c0637909b3f6a8d660dc
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-28 17:41:27 +0300

    try passs test

--- lesson-8/task-1/src/Life.jsx
+++ lesson-8/task-1/src/Life.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Life extends Component {
   constructor(props) {
     super(props);
+
     console.log('constructor: good place to create state');
   }
 
@@ -10,18 +11,14 @@ class Life extends Component {
     console.log('componentDidMount: API calls, subscriptions');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    // this.props, this.state
-    console.log(nextProps, nextState);
+  shouldComponentUpdate() {
     console.log(
-      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render',
+      'shoudlComponentUpdate(nextProps, nextState): decide to render or not to render',
     );
-    return nextProps.number % 2;
+    return true;
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    // this.props => будем работать с новыми props
-    // console.log(prevProps);
+  componentDidUpdate() {
     console.log(
       'componentDidUpdate(prevProps, prevState): some updates based on new props',
     );
@@ -35,7 +32,8 @@ class Life extends Component {
 
   render() {
     console.log('return React element to build DOM');
-    return <div className="number">{this.props.number}</div>;
+
+    return <div>1</div>;
   }
 }
 
@@ -57,3 +55,42 @@ export default Life;
 // componentWillUnmount()
 
 // Пользовательские методы обьявляем между render() и Методами жизненого цикла
+
+// class Life extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log('constructor: good place to create state');
+//   }
+
+//   componentDidMount() {
+//     console.log('componentDidMount: API calls, subscriptions');
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     // this.props, this.state
+//     console.log(nextProps, nextState);
+//     console.log(
+//       'shouldComponentUpdate(nextProps, nextState): decide to render or not to render',
+//     );
+//     return nextProps.number % 2;
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     // this.props => будем работать с новыми props
+//     // console.log(prevProps);
+//     console.log(
+//       'componentDidUpdate(prevProps, prevState): some updates based on new props',
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     console.log(
+//       'componentWillUnmount(): cleanup before DOM related to component will be removed',
+//     );
+//   }
+
+//   render() {
+//     console.log('return React element to build DOM');
+//     return <div className="number">{this.props.number}</div>;
+//   }
+// }
