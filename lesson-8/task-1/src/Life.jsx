d0527f99ebe0501b9dde1ab0cf37c26ec3e017df
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-28 17:32:11 +0300

    Методы жизненного цикла messages

--- lesson-8/task-1/src/Life.jsx
+++ lesson-8/task-1/src/Life.jsx
@@ -3,32 +3,38 @@ import React, { Component } from 'react';
 class Life extends Component {
   constructor(props) {
     super(props);
-    console.log('=> constructor');
+    console.log('constructor: good place to create state');
   }
 
   componentDidMount() {
-    console.log('=> componentDidMount');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     // this.props, this.state
     console.log(nextProps, nextState);
-    console.log('=> shouldComponentUpdate');
+    console.log(
+      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render',
+    );
     return nextProps.number % 2;
   }
 
   componentDidUpdate(prevProps, prevState) {
     // this.props => будем работать с новыми props
     console.log(prevProps);
-    console.log('=> componentDidUpdate');
+    console.log(
+      'componentDidUpdate(prevProps, prevState): some updates based on new props',
+    );
   }
 
   componentWillUnmount() {
-    console.log('=> componentWillUnmount');
+    console.log(
+      'componentWillUnmount(): cleanup before DOM related to component will be removed',
+    );
   }
 
   render() {
-    console.log('=> render');
+    console.log('return React element to build DOM');
     return <div className="number">{this.props.number}</div>;
   }
 }
