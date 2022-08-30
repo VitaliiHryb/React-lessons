221486d7285359d49584eeafb882501f23cb18e7
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-30 15:59:35 +0300

    App add

--- lesson-9/task-2/src/UserForm.jsx
+++ lesson-9/task-2/src/UserForm.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class UserForm extends React.Component {
+class UserForm extends Component {
   state = {
     name: '',
-    student: '',
+    student: false,
     occupation: '',
     about: '',
   };
 
-  handleChange = e => {
-    const { name, value, checked, type } = e.target;
-
-    const val = type === 'checkbox' ? checked : value;
+  handleChange = event => {
+    const { name, value, checked, type } = event.target;
+    const result = type === 'checkbox' ? checked : value;
 
     this.setState({
-      [name]: val,
+      [name]: result,
     });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
     console.log(this.state);
   };
 
@@ -36,8 +35,8 @@ class UserForm extends React.Component {
             type="text"
             id="name"
             name="name"
-            value={this.state.name}
             onChange={this.handleChange}
+            value={this.state.name}
           />
         </div>
         <div className="form-control">
@@ -46,11 +45,11 @@ class UserForm extends React.Component {
           </label>
           <input
             className="form-input"
-            value={this.state.student}
-            onChange={this.handleChange}
             type="checkbox"
             id="student"
             name="student"
+            onChange={this.handleChange}
+            value={this.state.student}
           />
         </div>
         <div className="form-control">
@@ -58,10 +57,10 @@ class UserForm extends React.Component {
             Occupation
           </label>
           <select
-            value={this.state.occupation}
-            onChange={this.handleChange}
             name="occupation"
             className="form-input"
+            onChange={this.handleChange}
+            value={this.state.occupation}
           >
             <option value="london">London</option>
             <option value="new-york">New York</option>
@@ -75,9 +74,9 @@ class UserForm extends React.Component {
           </label>
           <textarea
             name="about"
-            value={this.state.about}
-            onChange={this.handleChange}
             className="form-input"
+            onChange={this.handleChange}
+            value={this.state.about}
           />
         </div>
         <button className="submit-button" type="submit">
@@ -89,31 +88,3 @@ class UserForm extends React.Component {
 }
 
 export default UserForm;
-
-/*
-<form className="login-form">
-  <h1 className="form-title">Profile</h1>
-  <div className="form-control">
-    <label className="form-label" htmlFor="name">Name</label>
-    <input className="form-input" type="text" id="name" name="name" value="">
-  </div>
-  <div className="form-control">
-    <label className="form-label" htmlFor="student">Student</label>
-    <input className="form-input" type="checkbox" id="student" name="student">
-  </div>
-  <div className="form-control">
-    <label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
-    <select name="occupation" className="form-input">
-      <option value="london">London</option>
-      <option value="new-york">New York</option>
-      <option value="coconut">Sidney</option>
-      <option value="mango">Berlin</option>
-    </select>
-  </div>
-  <div className="form-control">
-    <label className="form-label" id="about" htmlFor="about">About</label>
-    <textarea name="about" className="form-input"></textarea>
-  </div>
-  <button className="submit-button" type="submit">Submit</button>
-</form>
-*/
--- lesson-9/task-2/src/index.jsx
+++ lesson-9/task-2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import UserForm from './UserForm';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<UserForm />);
+root.render(<App />);
