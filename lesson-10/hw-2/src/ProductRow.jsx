64c9a60e4378401bc7e5425581a0899cf9b3e819
VitaliiHryb <vitaliigryb@ukr.net>
2022-09-02 19:31:20 +0300

    Список товаров base version

---
+++ lesson-10/hw-2/src/ProductRow.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+// ProductRow (red): displays a row for each product
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? (
+      product.name
+    ) : (
+      <span style={{ color: 'red' }}>{product.name}</span>
+    );
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+export default ProductRow;
+
+// The legend tag is used to define the title for the child contents.
+// This tag is used to define the caption for the <fieldset> element.
+
+// mooved to Calculator
+/*
+constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: '' };
+  }
+
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+*/
